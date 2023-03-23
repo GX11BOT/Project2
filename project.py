2604e280918720da73a6e175b45010ba531c123f
ARNAV NEGI <negijiarnav@gmail.com>
2023-03-23 09:21:40 +0530

    second commit

---
+++ project.py
@@ -0,0 +1,44 @@
+def connection():
+    try:
+        conn=sqlite3.connect("student.db")
+    except:
+        print("cannot connect to the database")
+    return conn    
+
+
+def verifier():
+    a=b=c=d=e=f=0
+    if not student_name.get():
+        t1.insert(END,"<>Student name is required<>\n")
+        a=1
+    if not roll_no.get():
+        t1.insert(END,"<>Roll no is required<>\n")
+        b=1
+    if not branch.get():
+        t1.insert(END,"<>Branch is required<>\n")
+        c=1
+    if not phone.get():
+        t1.insert(END,"<>Phone number is requrired<>\n")
+        d=1
+    if not father.get():
+        t1.insert(END,"<>Father name is required<>\n")
+        e=1
+    if not address.get():
+        t1.insert(END,"<>Address is Required<>\n")
+        f=1
+    if a==1 or b==1 or c==1 or d==1 or e==1 or f==1:
+        return 1
+    else:
+        return 0
+
+
+def add_student :
+            ret=verifier()
+            if ret==0:
+                conn=connection()
+                cur=conn.cursor()
+                cur.execute("CREATE TABLE IF NOT EXISTS STUDENTS(NAME TEXT,ROLL_NO INTEGER,BRANCH TEXT,PHONE_NO INTEGER,FATHER TEXT,ADDRESS TEXT)")
+                cur.execute("insert into STUDENTS values(?,?,?,?,?,?)",(student_name.get(),int(roll_no.get()),branch.get(),int(phone.get()),father.get(),address.get()))
+                conn.commit()
+                conn.close()
+                t1.insert(END,"ADDED SUCCESSFULLY\n")
